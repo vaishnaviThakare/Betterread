088281229d7854d931ebb507e1c6e5dd99bb434d
vaishnavi <vaishnavithakre03@gmail.com>
2024-03-27 12:55:11 +0530

    Starter Auth setup for better Reads

---
+++ src/main/java/io/javabrains/SecurityAdapter.java
@@ -0,0 +1,36 @@
+package io.javabrains;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class SecurityAdapter {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.authorizeRequests(a -> a
+				.antMatchers("/", "/error").permitAll()
+				.anyRequest().authenticated()
+			)
+			.exceptionHandling(e -> e
+				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+			)
+			.csrf(c -> c
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			)
+			.logout(l -> l
+				.logoutSuccessUrl("/").permitAll()
+			)
+			.oauth2Login();
+        return http.build();
+		// @formatter:on
+    }
+    
+}
